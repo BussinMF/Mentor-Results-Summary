19eb33141c978aa3d28cb1e1e8ac43143601a1c1
BussinMF <133649620+BussinMF@users.noreply.github.com>
2023-11-03 18:41:35 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bussinmf.github.io/Mentor-Results-Summary/',
+  base: '/Mentor-Results-Summary/',
   plugins: [react()],
 })
